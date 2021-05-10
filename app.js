86e36f6ecae44eeb07e41fa9bb36f18987864fb5
catriel10 <catriel10@gmail.com>
2021-05-10 11:37:06 -0300

    Logos y HTML

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ("express");
+const app = express ();
+const path = require ("path");
+
+app.use(express.static("public"))
+
+app.listen (4444, ()=>{
+console.log ("Mi servidor GRUPO 4 esta funcionando en port 4444")
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/src/index.html'));
+    });
