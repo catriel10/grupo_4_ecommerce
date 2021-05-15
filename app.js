17fa8cd2078c0fbfb014cca09b337e51fe613452
catriel10 <catriel10@gmail.com>
2021-05-14 21:22:47 -0300

    trabajo en equipo 14/5

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/src/productDetail.html'));
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/src/login.html'));
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/src/register.html'));
     });
