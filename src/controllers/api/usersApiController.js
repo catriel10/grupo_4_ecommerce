4ced811a7d212cee928bc5ad42346f82a08ffa91
stefiian <stefilorean@gmail.com>
2021-09-09 19:34:29 -0300

    api!

---
+++ src/controllers/api/usersApiController.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const usersRoutes = express.Router()
+const usersApiController = require('../../controllers/api/usersApiController')
+
+
+
+
+
+
+module.exports = usersApiController
--- src/routes/api/usersApiRoutes.js
+++ src/routes/api/usersApiRoutes.js
@@ -1,9 +1,9 @@
 const express = require ('express')
 const usersApiRoutes = express.Router()
-const usersApiController = require ('../../controllers/api/usersApiController')
+const usersApiController = require('../../controllers/api/usersApiController')
 
 
-
+usersApiRoutes.get('/', usersApiController.)
 
 
 
